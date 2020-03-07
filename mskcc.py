c7e635fe5fe7a7d84b1a7b8d7f10ebd3c8c7db28
Delaunay Julien <julien.delaunay@insa-rouen.fr>
2020-03-07 09:46:31 +0100

    Ajout de l env virtuel + mskcc.py

---
+++ mskcc.py
@@ -0,0 +1,17 @@
+import json
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
+class Database:
+
+    param_username = ""
+    param_password = ""
+    url_connection = ""
+    param_herb = ""
+    param_drug = ""
+    url_results = ""
+
+    def __init__(self):
+        pass
