a88aa8aa856bc61033e9284a1d33823b886d25b8
Léo Pacary <leo@pacary.eu>
2020-03-07 16:27:35 +0100

    ajout methode mskcc

--- mskcc.py
+++ mskcc.py
@@ -4,6 +4,7 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+
 class Database:
 
     param_username = ""
@@ -16,6 +17,7 @@ class Database:
     def __init__(self):
         pass
 
+
 class MSKCC(Database):
 
     drugs = dict()
@@ -25,35 +27,36 @@ class MSKCC(Database):
     file_encoding = "utf8"
     param_herb = "data[htinteraction][herb_id]"
     param_drug = "data[dtinteraction][drug_id]"
-    url_results = "https://hedrine.univ-grenoble-alpes.fr/htinteractions/hdi"
+    url_results = "https://www.mskcc.org/cancer-care/diagnosis-treatment/symptom-management/integrative-medicine/herbs/search"
 
     def __init__(self):
         super().__init__()
 
+
     @classmethod
-    def get_drugs_names(cls, connection):
-        base_url = "https://hedrine.univ-grenoble-alpes.fr/drugs/view"
-        nb_max = 659
-        for index in range(1, nb_max+1):
-            url = f"{base_url}/{index}"
-            request = connection.session.get(url)
-            html = BeautifulSoup(request.content, "html.parser")
-            try:
-                name = html.select(".drugs.view dl dd")[0]
-                Hedrine.drugs[index] = name.text.strip()
-            except IndexError:
-                pass
+    def get_nb_herbs(cls):
+        page = requests.get(cls.url_results)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        nb_herbs = soup.find_all('span', class_='msk-filtered-results__num')
+        nb_herbs = list(nb_herbs)[0]
+        return nb_herbs.get_text()
 
     @classmethod
     def get_herbs_names(cls, connection):
-        base_url = "https://hedrine.univ-grenoble-alpes.fr/herbs/view"
-        nb_max = 201
-        for index in range(1, nb_max+1):
-            url = f"{base_url}/{index}"
-            request = connection.session.get(url
-            html = BeautifulSoup(request.content, "html.parser")
-            try:
-                name = html.select(".herbs.view dl dd")[0]
-                Hedrine.herbs[index] = name.text.strip()
-            except IndexError:
-                pass
+        herb_name = []
+        nbherb = cls.get_nb_herbs()
+        for i in range(nbherb // 10 + 1):
+            page = requests.get(cls.baseUrl + "?page=" + str(i))
+            soup = BeautifulSoup(page.content, 'html.parser')
+            proteines_price_from = soup.find_all('a', class_='baseball-card__link')
+            tmp_herbs = [val.text.strip() for val in proteines_price_from]
+            herb_name.extend(tmp_herbs)
+        return herb_name
+
+    @classmethod
+    def get_herbs_other_name(cls,herbCible):
+        urlHerbsBase = "https://www.mskcc.org/cancer-care/integrative-medicine/herbs/"
+        page = requests.get(urlHerbsBase + herbCible.replace(" ", "-"))
+        soup = BeautifulSoup(page.content, 'html.parser')
+        listapuce = soup.select('.list-bullets li')
+        return [val.get_text() for val in listapuce]
